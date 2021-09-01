994bf02bb7fe7b96733a819984dccae9b370ce6b
Leon <leopard10000@gmail.com>
2021-09-01 11:06:47 +0900

    Vuexストアの getters

--- src/App.vue
+++ src/App.vue
@@ -10,28 +10,59 @@
     <br>
     <br>
     {{ someOtherProp }}
+    <br>
+    Books count: {{booksCount}}
+    <br>
+    getBooksByPrice: {{getBooksByPrice(3000)}}
+    <br>
+    booksCount3000: {{booksCount3000}}
+    <br>
+
+    <form v-on:submit.prevent="onClick">
+      <label for="isbn">ISBN: </label>
+      <input type="text" id="isbn" v-model="isbn"><br>
+      <label for="title">書名: </label>
+      <input type="text" id=title v-model="title"><br>
+      <label for="price">価格: </label>
+      <input type="number" id="price" v-model="price"><br>
+      <input type="submit" value="登録">
+    </form>
+
+    <br>
+    <div v-for="book in books" v-bind:key="book.isbn">
+      <p>{{book.title}}</p>
+      <p>{{book.price}}</p>
+      <p>{{book.isbn}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   name: "app",
   components: {},
   props: {},
   data: function () {
-    return {};
+    return {
+      isbn: '',
+      title: '',
+      price: 0
+    };
   },
   computed: {
     someOtherProp() { return 1},
     // Vuexの count を count にマッピング （いちいちcount computedメソッドを作る必要が無くなる）
-    ...mapState(['count']),
+    ...mapState(['count', 'books']),
     // 別名でマッピング
     ...mapState({ countNumber: 'count' }),
     // count() {
     //   return this.$store.state.count;
     // },
+
+    // VUexの getters を同名の算出プロパティにmapする
+    ...mapGetters([ 'booksCount', 'getBooksByPrice', 'booksCount3000'])
   },
   mounted: function () {},
   methods: {
@@ -42,6 +73,15 @@ export default {
     plus() {
       this.$store.commit("plus");
     },
+    onClick(){
+      this.$store.commit('addBook', {
+        book: {
+          isbn: this.isbn,
+          title: this.title,
+          price: this.price
+        }
+      })
+    }
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,19 @@ export default new Vuex.Store({
   // ただし、オーバーヘッドが大きいので本番環境ではfalseにすること。
   strict: true,
   state: {
-    count: 0
+    count: 0,
+    books: [
+      {
+        isbn: '987-4-8222-5389-9',
+        title: '作って楽しむプログラミング HTML5超入門',
+        price: 2000
+      },
+      {
+        isbn: '999-4-8222-5389-9',
+        title: 'Vue.js超入門',
+        price: 3456
+      },
+    ]
   },
   mutations: {
     // state変更用のメソッド
@@ -17,6 +29,27 @@ export default new Vuex.Store({
     },
     plus(state){
       state.count++
+    },
+    // 引数も渡せる ※payloadインスタンスは以下の軽視になっていると仮定 { book: {} } 
+    addBook(state, payload){
+      state.books.push(payload.book)
+    }
+  },
+  getters: {
+    // 何冊のbookがあるかを返す ※結果はキャッシュされる
+    booksCount(state){
+      return state.books.length
+    },
+    // 「引数に price を受け取り、それ未満のbook配列を返す」関数を返す
+    // ※結果はキャッシュされない
+    getBooksByPrice(state){
+      return price => {
+        return state.books.filter(book => book.price < price)
+      }
+    },
+    // 他のgetterを呼び出すこともできる
+    booksCount3000(state, getters){
+      return getters.getBooksByPrice(3000).length;
     }
   },
   actions: {
